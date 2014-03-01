bfd7a1f2b281248098dcd05ac130ec0f5eaecbd0
zhul6 <zhulitie@gmail.com>
2014-02-28 21:56:16 -0500

    made login activity
    made a very basic login activity for project-shediao.
    it is only a interface, does not possess any functionality yet.

--- SheDiao/gen/com/example/shediao/R.java
+++ SheDiao/gen/com/example/shediao/R.java
@@ -24,7 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LoginButton=0x7f080004;
+        public static final int action_settings=0x7f080006;
+        public static final int passwordEditText=0x7f080001;
+        public static final int signUpButton=0x7f080003;
+        public static final int smallTitleTextView=0x7f080005;
+        public static final int titleTextView=0x7f080000;
+        public static final int userNameEditText=0x7f080002;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -35,7 +41,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int login_button=0x7f050006;
+        public static final int password=0x7f050003;
+        public static final int signup_button=0x7f050007;
+        public static final int small_title=0x7f050005;
+        public static final int title=0x7f050004;
+        public static final int user_name=0x7f050002;
     }
     public static final class style {
         /** 
--- SheDiao/src/com/example/shediao/Login.java
+++ SheDiao/src/com/example/shediao/Login.java
@@ -1,6 +1,7 @@
 package com.example.shediao;
 
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.view.Menu;
 
@@ -10,8 +11,10 @@ public class Login extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
