b00577d0d31194b1a8c4fbbe665a7ce61ef78801
zhul6 <zhulitie@gmail.com>
2014-03-21 18:55:46 -0400

    improve login UI

---
+++ SheDiao/src/com/example/shediao/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.shediao;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Handler;
+import android.content.Intent;
+
+public class SplashActivity extends Activity {
+
+	private Intent intent;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		startMainActivity();
+	}
+
+	private void startMainActivity(){
+		new Handler().postDelayed(new Runnable() {
+			public void run() {
+				intent = new Intent(SplashActivity.this, Login.class);
+				startActivity(intent);
+				SplashActivity.this.finish();
+			}
+		}, 1000);
+	}
+
+}
